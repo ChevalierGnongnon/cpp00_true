40e5970ad606acbdf85d80bbed7f3ee030bcd0f7
chhoflac <chhoflac@student.42.fr>
2025-02-10 10:30:22 +0100

    getters and setters progress

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,17 +6,11 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:18:10 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/03 19:56:35 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/10 10:28:35 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
-
-Contact contact(const std::string firstName, const std::string lastName, \
-	const std::string nickName, const std::string phoneNumber, const std::string darkestSecret)
-{
-	
-}
 
 std::string	Contact::getFirstName(){
 	return (this->first_name);
@@ -42,19 +36,19 @@ void	Contact::setFirstName(const std::string &firstName){
 	this->first_name = firstName;
 }
 
-void	Contact::setLastName(const std::string &firstName){
-	this->first_name = firstName;
+void	Contact::setLastName(const std::string &lastName){
+	this->last_name = lastName;
 }
 
-void	Contact::setNickName(const std::string &firstName){
-	this->first_name = firstName;
+void	Contact::setNickName(const std::string &nickName){
+	this->nick_name = nickName;
 }
 
-void	Contact::setPhoneNumber(const std::string &firstName){
-	this->first_name = firstName;
+void	Contact::setPhoneNumber(const std::string &phoneNumber){
+	this->phone_number = phoneNumber;
 }
 
-void	Contact::setDarkestSecret(const std::string &firstName){
-	this->first_name = firstName;
+void	Contact::setDarkestSecret(const std::string &darkestSecret){
+	this->darkest_secret = darkestSecret;
 }
 
