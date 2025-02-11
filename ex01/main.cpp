572c3d2a33f33ed8beaf05ca94388fa16eb47583
chhoflac <chhoflac@student.42.fr>
2025-02-11 10:35:52 +0100

    main in progress

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:11:20 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/11 10:21:43 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/11 10:34:10 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 int main(void)
 {
-	std::cout << "Welcome to phonebook !";
+	PhoneBook Book;
+	std::string input;
+	std::cout << "Welcome to phonebook !" << std::endl;
+
+	std::cin >> input;
+	while (input != "EXIT")
+	{
+		std::cout << "Please enter a command" << std::endl;
+		if (input == "ADD")
+			std::cout << "add has been selected" << std::endl;
+		else if (input == "SEARCH")
+			std::cout << "search has been selected" << std::endl;
+		else
+			std::cout << "Bad entry, please retry" << std::endl;
+		std::cin >> input;
+	}
 }
