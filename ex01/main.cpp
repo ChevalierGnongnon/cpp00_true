d798b05d75654940d1a8703c907105de937d075d
chhoflac <chhoflac@student.42.fr>
2025-02-14 18:05:27 +0100

    update line empty or invalid

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:11:20 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/14 16:03:11 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/14 18:04:59 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,27 @@ void create_contact(PhoneBook *Book, int index){
 	std::string input;
 	
 	std::cout << "Please enter contact first name :" << std::endl;
-	std::cin >> input;
+	std::getline(std::cin, input);
 	if (input.size() >= 1){
 		newContact->setFirstName(input);
 		std::cout << "Please enter contact last name :" << std::endl;
-		std::cin >> input;
+		std::getline(std::cin, input);
 		if (input.size() >= 1){
 			newContact->setLastName(input);
 			std::cout << "Please enter contact nick name :" << std::endl;
-			std::cin >> input;
+			std::getline(std::cin, input);
 			if (input.size() >= 1){
 				newContact->setNickName(input);
 				std::cout << "Please enter contact phone number :" << std::endl;
-				std::cin >> input;
+				std::getline(std::cin, input);
 				if (input.size() >= 1 && CheckInputIsNum(input)){
 					newContact->setPhoneNumber(input);
 					std::cout << "Please enter contact darkest secret :"  << std::endl;
-					std::cin >> input;
+					std::getline(std::cin, input);
 					if (input.size() >= 1){
 						newContact->setDarkestSecret(input);
 						std::cout << "Contact successfully registered." << std::endl;
 						Book->addContact(newContact);
-						Book->display();
 					}
 				}
 			}
@@ -73,10 +72,12 @@ int main(void)
 	int 		index;
 	
 	std::cout << "Welcome to phonebook !" << std::endl << "Please enter a command:" << std::endl;
-	std::cin >> command;
+	std::getline(std::cin, command);
 	while (command != "EXIT" && 1)
 	{
-		if (command == "ADD")
+		if (command.size() == 0 && command != "SEARCH" && command != "ADD")
+			std::cout << "Error: line is empty or invalid" << std::endl;
+		else if (command == "ADD")
 			create_contact(&Book, Book.get_nbContacts());
 		else if (command == "SEARCH")
 		{
@@ -85,7 +86,7 @@ int main(void)
 				std::cout << "Error: no contact are registered." << std::endl;
 			else {
 				std::cout << "please enter an index :" << std::endl;
-				std::cin >> test;
+				std::getline(std::cin, test);
 				if (!CheckInputIsNum(test))
 					std::cout << "Error: wrong input. Please enter a number." << std::endl;
 				else {
@@ -94,9 +95,8 @@ int main(void)
 			}
 		}
 		else
-			std::cout << "Bad entry, please retry" << std::endl;
+			std::cout << "bad entry" << std::endl;
 		std::cout << "Please enter a command:"<< std::endl;
-		std::cin >> command;
+		std::getline(std::cin, command);
 	}
-	Book.display();
 }
